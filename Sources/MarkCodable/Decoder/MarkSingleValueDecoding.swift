812481f88605a5db05531245118c5312f9ccfd36
Marin Todorov <marin@underplot.com>
2022-09-07 07:58:25 +0200

    removes redundand methods that could be handled by generics

--- Sources/MarkCodable/Decoder/MarkSingleValueDecoding.swift
+++ Sources/MarkCodable/Decoder/MarkSingleValueDecoding.swift
@@ -22,20 +22,9 @@ struct MarkSingleValueDecoding: SingleValueDecodingContainer {
         return value == "nil"
     }
 
-    func decode(_ type: Bool.Type) throws -> Bool { try unbox() }
-    func decode(_ type: String.Type) throws -> String { try unbox() }
-    func decode(_ type: Double.Type) throws -> Double { try unbox() }
-    func decode(_ type: Float.Type) throws -> Float { try unbox() }
-    func decode(_ type: Int.Type) throws -> Int { try unbox() }
-    func decode(_ type: Int8.Type) throws -> Int8 { try unbox() }
-    func decode(_ type: Int16.Type) throws -> Int16 { try unbox() }
-    func decode(_ type: Int32.Type) throws -> Int32 { try unbox() }
-    func decode(_ type: Int64.Type) throws -> Int64 { try unbox() }
-    func decode(_ type: UInt.Type) throws -> UInt { try unbox() }
-    func decode(_ type: UInt8.Type) throws -> UInt8 { try unbox() }
-    func decode(_ type: UInt16.Type) throws -> UInt16 { try unbox() }
-    func decode(_ type: UInt32.Type) throws -> UInt32 { try unbox() }
-    func decode(_ type: UInt64.Type) throws -> UInt64 { try unbox() }
+    func decode<T>(_ type: T.Type) throws -> T where T : Decodable, T: StringInitializable {
+        return try unbox()
+    }
 
     func decode<T>(_ type: T.Type) throws -> T where T : Decodable {
         switch T.self {
--- Sources/MarkCodable/Encoder/MarkKeyedEncoding.swift
+++ Sources/MarkCodable/Encoder/MarkKeyedEncoding.swift
@@ -23,60 +23,8 @@ struct MarkKeyedEncoding<Key: CodingKey>: KeyedEncodingContainerProtocol {
         data.encode(key: codingPath + [key], value: "")
     }
 
-    mutating func encode(_ value: Bool, forKey key: Key) throws {
-        data.encode(key: codingPath + [key], value: String(value))
-    }
-
-    mutating func encode(_ value: String, forKey key: Key) throws {
-        data.encode(key: codingPath + [key], value: value)
-    }
-
-    mutating func encode(_ value: Double, forKey key: Key) throws {
-        data.encode(key: codingPath + [key], value: String(value))
-    }
-
-    mutating func encode(_ value: Float, forKey key: Key) throws {
-        data.encode(key: codingPath + [key], value: String(value))
-    }
-
-    mutating func encode(_ value: Int, forKey key: Key) throws {
-        data.encode(key: codingPath + [key], value: String(value))
-    }
-
-    mutating func encode(_ value: Int8, forKey key: Key) throws {
-        data.encode(key: codingPath + [key], value: String(value))
-    }
-
-    mutating func encode(_ value: Int16, forKey key: Key) throws {
-        data.encode(key: codingPath + [key], value: String(value))
-    }
-
-    mutating func encode(_ value: Int32, forKey key: Key) throws {
-        data.encode(key: codingPath + [key], value: String(value))
-    }
-
-    mutating func encode(_ value: Int64, forKey key: Key) throws {
-        data.encode(key: codingPath + [key], value: String(value))
-    }
-
-    mutating func encode(_ value: UInt, forKey key: Key) throws {
-        data.encode(key: codingPath + [key], value: String(value))
-    }
-
-    mutating func encode(_ value: UInt8, forKey key: Key) throws {
-        data.encode(key: codingPath + [key], value: String(value))
-    }
-
-    mutating func encode(_ value: UInt16, forKey key: Key) throws {
-        data.encode(key: codingPath + [key], value: String(value))
-    }
-
-    mutating func encode(_ value: UInt32, forKey key: Key) throws {
-        data.encode(key: codingPath + [key], value: String(value))
-    }
-
-    mutating func encode(_ value: UInt64, forKey key: Key) throws {
-        data.encode(key: codingPath + [key], value: String(value))
+    mutating func encode<T: Encodable & StringInitializable>(_ value: T, forKey key: Key) throws {
+        data.encode(key: codingPath + [key], value: String(describing: value))
     }
 
     mutating func encode<T: Encodable>(_ value: T, forKey key: Key) throws {
